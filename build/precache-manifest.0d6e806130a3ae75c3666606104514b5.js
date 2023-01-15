109ff6f31e5e853adce83a2df4e6874309e38aba
adolawuyi <adolawuyi@gmail.com>
2023-01-15 14:52:47 +0100

    update

---
+++ build/precache-manifest.0d6e806130a3ae75c3666606104514b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "027360badb6e2306fc15",
+    "url": "/criberr-deploy/static/css/main.c51206d0.chunk.css"
+  },
+  {
+    "revision": "027360badb6e2306fc15",
+    "url": "/criberr-deploy/static/js/main.027360ba.chunk.js"
+  },
+  {
+    "revision": "458fe5aedc881f1722ac",
+    "url": "/criberr-deploy/static/css/1.ce54c784.chunk.css"
+  },
+  {
+    "revision": "458fe5aedc881f1722ac",
+    "url": "/criberr-deploy/static/js/1.458fe5ae.chunk.js"
+  },
+  {
+    "revision": "0992be1fbeb249ea255a",
+    "url": "/criberr-deploy/static/js/runtime~main.0992be1f.js"
+  },
+  {
+    "revision": "95769e6eb47a26eaf4a7b217f5c0d19f",
+    "url": "/criberr-deploy/static/media/amazon.95769e6e.mp4"
+  },
+  {
+    "revision": "2ef96d2b58521ce3a2b9655f4e861fd0",
+    "url": "/criberr-deploy/static/media/logo.2ef96d2b.png"
+  },
+  {
+    "revision": "6d83c4986bc074190da710d395dcd893",
+    "url": "/criberr-deploy/static/media/imghero.6d83c498.png"
+  },
+  {
+    "revision": "aaba630680c92c14c2cfe64ebe4ebf31",
+    "url": "/criberr-deploy/static/media/headerbg.aaba6306.png"
+  },
+  {
+    "revision": "3f1105a7c2166378e8063591f68189f4",
+    "url": "/criberr-deploy/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9fd91cc972f9bf9654aaa2dad104893.js"
+  "/criberr-deploy/precache-manifest.0d6e806130a3ae75c3666606104514b5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/criberr-deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
